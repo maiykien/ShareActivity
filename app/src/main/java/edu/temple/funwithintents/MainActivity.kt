9bd24e454ca3988a10837eb958c4ac05f890e60a
ykienmai <maikienit@gmail.com>
2026-03-31 12:10:51 -0400

    Finished the fun with intents under professor's instruction on March 31, 2026

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -5,6 +5,9 @@ import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
+import android.content.Intent
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,8 +16,31 @@ class MainActivity : AppCompatActivity() {
         // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
 
+
+
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+
+            // Open the text to show the text string.
+            var text_String = editText.text.toString()
+
+            if (text_String.isEmpty()) {
+                return@setOnClickListener
+            }
+
+
+            val sendIntent = Intent()
+
+            sendIntent.action = Intent.ACTION_SEND
+
+            sendIntent.putExtra(Intent.EXTRA_TEXT, text_String)
+            sendIntent.type = "text/plain"
+
+            if (sendIntent.resolveActivity(packageManager) != null) {
+
+                startActivity(sendIntent)
+
+            }
 
         }
     }
